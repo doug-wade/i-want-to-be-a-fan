98447f1dc004fb84c10c18aa3def9e99d6f310ea
Douglas Wade <douglas.b.wade@gmail.com>
2024-03-08 20:15:31 -0800

    feat(#32): Scrape us soccer's website to get the roster

---
+++ _data/roster.js
@@ -0,0 +1,38 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const jsdom = require("jsdom");
+
+const { JSDOM } = jsdom;
+
+module.exports = async function() {
+  const url = "https://www.ussoccer.com/teams/uswnt/roster";
+  const response = await EleventyFetch(url, {
+    duration: "1d",
+    type: "text"
+  });
+
+  const dom = new JSDOM(response);
+  const playerDivs = dom.window.document.querySelectorAll("div.col.col--xs-6.col--sm-4.col--md-3");
+
+  console.log(playerDivs.length)
+
+  return Array.from(playerDivs).map((playerDiv) => {
+    const nameAndNumber = playerDiv.querySelector("div.PlayerThumbnail-module__playerName--2bbtZ");
+    const nameAndNumberArray = nameAndNumber.innerHTML.split(/<!-- --> <!-- --> <!-- -->/);
+    const number = nameAndNumberArray.pop();
+    const name = nameAndNumberArray.join(" ");
+
+    const anchor = playerDiv.querySelector("a");
+    const link = anchor.getAttribute("href");
+
+    const img = playerDiv.querySelector("img");
+    const imgSrc = img.getAttribute("src");
+
+    return {
+        name,
+        number,
+        link,
+        ussoccerLink: link,
+        img: imgSrc
+    }
+  })
+}
